f6aa7dea86ad5ae684724848a180312747e176fa
agusmachado <agusmachado@gmail.com>
2023-11-28 15:54:07 -0300

    Primera parte del backend finalizada

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -13,7 +13,7 @@ const agregarTarea = async (req, res) =>{
     
     if (existeProyecto.creador.toString() !== req.usuario._id.toString()) {
         const error = new Error("No tienes los permisos para añadir tareas")
-        return res.status(404).json({ msg: error.message })
+        return res.status(403).json({ msg: error.message })
     }
 
     try {
@@ -24,11 +24,76 @@ const agregarTarea = async (req, res) =>{
     }
 }
 
-const obtnerTarea = async (req, res) =>{}
+const obtnerTarea = async (req, res) =>{
+    const { id } = req.params
 
-const actualizarTarea = async (req, res) =>{}
+    const tarea = await Tarea.findById(id).populate("proyecto")
 
-const eliminarTarea = async (req, res) =>{}
+    if (!tarea) {
+        const error = new Error("Tarea no encontrada")
+        return res.status(404).json({ msg: error.message })
+    }
+
+    if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("Acción no válida")
+        return res.status(403).json({ msg: error.message })
+    }
+    res.json(tarea)
+}
+
+const actualizarTarea = async (req, res) =>{
+    const { id } = req.params
+
+    const tarea = await Tarea.findById(id).populate("proyecto")
+
+    if (!tarea) {
+        const error = new Error("Tarea no encontrada")
+        return res.status(404).json({ msg: error.message })
+    }
+
+    if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("Acción no válida")
+        return res.status(403).json({ msg: error.message })
+    }
+
+    tarea.nombre = req.body.nombre || tarea.nombre
+    tarea.descripcion = req.body.descripcion || tarea.descripcion
+    tarea.prioridad = req.body.prioridad || tarea.prioridad
+    tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega
+
+    try {
+        const tareaAlmacenada = await tarea.save()
+        res.json(tareaAlmacenada)
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+const eliminarTarea = async (req, res) =>{
+    const { id } = req.params
+
+    const tarea = await Tarea.findById(id).populate("proyecto")
+
+    if (!tarea) {
+        const error = new Error("Tarea no encontrada")
+        return res.status(404).json({ msg: error.message })
+    }
+
+    if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("Acción no válida")
+        return res.status(403).json({ msg: error.message })
+    }
+
+    try {
+        await tarea.deleteOne()
+        res.json({ msg: 'Tarea Eliminada'})
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 const cambiarEstado = async (req, res) =>{}
 
--- routes/proyectoRoutes.js
+++ routes/proyectoRoutes.js
@@ -7,8 +7,7 @@ import {
     editarProyecto,
     eliminarProyecto,
     agregarColaborador,
-    eliminarColaborador,
-    obtenerTareas
+    eliminarColaborador
 } from '../controllers/proyectoController.js'
 
 import checkAuth from '../middleware/checkAuth.js'
@@ -25,8 +24,7 @@ router
     .get(checkAuth, obtenerProyecto)
     .put(checkAuth, editarProyecto)
     .delete(checkAuth, eliminarProyecto)
-
-router.get('/tareas/:id', checkAuth, obtenerTareas)
+    
 router.post('/agregar-colaborador/:id', checkAuth, agregarColaborador)
 router.post('/eliminar-colaborador/:id', checkAuth, eliminarColaborador)
 
